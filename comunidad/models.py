eba2ac05d63603dda94f2e8d43f9e5c71e0e6ecd
JampiBryan <jampibryan@gmail.com>
2024-08-25 20:45:40 -0500

    modelo disciplina y publicacion

--- comunidad/models.py
+++ comunidad/models.py
@@ -20,4 +20,24 @@ class Usuario(models.Model):
     def __str__(self):
         return f"{self.nombres} {self.apellidos}"
     
+
+class Disciplina(models.Model):
+    descripcionDisciplina = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descripcionDisciplina
     
+    
+class Publicacion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.PROTECT)
+    fechaPublicacion = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=False)
+    mensaje = models.TextField(blank=True, null=False)
+
+    def __str__(self):
+        return f"Publicación de {self.usuario} en {self.disciplina}"
+
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
